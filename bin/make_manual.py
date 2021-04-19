wang666233-art/rclone

commit a954e1172189d496bf677909034a528f762ccb7a

uptobox: add docs

bin/make_manual.py
@@ -62,6 +62,7 @@ docs = [
     "sftp.md",
     "sugarsync.md",
     "tardigrade.md",
+    "uptobox.md"
     "union.md",
     "webdav.md",
     "yandex.md",

docs/layouts/chrome/navbar.html
@@ -98,6 +98,7 @@
           <a class="dropdown-item" href="/sftp/"><i class="fa fa-server"></i> SFTP</a>
           <a class="dropdown-item" href="/sugarsync/"><i class="fas fa-dove"></i> SugarSync</a>
           <a class="dropdown-item" href="/tardigrade/"><i class="fas fa-dove"></i> Tardigrade</a>
+          <a class="dropdown-item" href="/uptobox/"><i class="fa fa-archive"></i> Uptobox</a>
           <a class="dropdown-item" href="/union/"><i class="fa fa-link"></i> Union (merge backends)</a>
           <a class="dropdown-item" href="/webdav/"><i class="fa fa-server"></i> WebDAV</a>
           <a class="dropdown-item" href="/yandex/"><i class="fa fa-space-shuttle"></i> Yandex Disk</a>

commit f607aa7c86b3b2a7a5eb5fc430cc8a3277f9b00b

make_manual.py: fix missing comma for doc build after uptobox merge
This fixes a problem introduced in

a954e11721 uptobox: add docs

bin/make_manual.py
@@ -62,7 +62,7 @@ docs = [
     "sftp.md",
     "sugarsync.md",
     "tardigrade.md",
-    "uptobox.md"
+    "uptobox.md",
     "union.md",
     "webdav.md",
     "yandex.md",